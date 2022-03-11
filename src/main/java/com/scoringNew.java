e5a1442cd86c4f876299bde25aeaa2e0cd7f7b66
frankshi <frankshicar@gmail.com>
2022-03-11 14:29:34 +0800

    initial commit

--- src/main/java/com/scoringNew.java
+++ src/main/java/com/scoringNew.java
@@ -2,9 +2,12 @@ package com;
 
 public class scoringNew {
     public static void main(String[] args) {
-        Student jack = new Student();
-        jack.name = "Jack";
+        Student jack = new Student("jack");
+//        jack.name = "Jack";
         jack.english = 70;
         jack.math = 88;
+        Student hack = new Student("Hank",60,30);
+        jack.print();
+        hack.print();
     }
 }
