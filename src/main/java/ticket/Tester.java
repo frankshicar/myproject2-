fada48ef7064fda6949dcf81bf47a94b1f80e373
frankshi <frankshicar@gmail.com>
2022-04-01 16:31:34 +0800

    initial commit

--- src/main/java/ticket/Tester.java
+++ src/main/java/ticket/Tester.java
@@ -1,16 +1,73 @@
 package ticket;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Tester {
     public static void main(String[] args) {
         //Constructor, constant value
-        Ticket ticket = new Ticket(
-                Station.TAIPEI_STATION,
-                Station.KAOHSIUNG_STATION);
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Your start station?(1: Taipei, 2: Taichung, 3: Kaohsiung)");
+//        Station start = null;
+//        int startChoice=Integer.parseInt(scanner.next());
+//        switch (startChoice){
+//            case 1:
+//                start = Station.TAIPEI_STATION;
+//                break;
+//            case 2:
+//                start = Station.TAICHUNG_STATION;
+//            case 3:
+//                start = Station.KAOHSIUNG_STATION;
+//        }
+//        Scanner scanner2 = new Scanner(System.in);
+//        System.out.println("You destination station?[1]Taipei, [2]Taichung, [3]KAOHSIUNG");
+//        Station destination=null;
+//        int destinationChoice=Integer.parseInt(scanner2.next());
+//        switch (destinationChoice){
+//            case 1:
+//                destination= Station.TAIPEI_STATION;
+//                break;
+//            case 2:
+//                destination= Station.TAICHUNG_STATION;
+//                break;
+//            case 3:
+//                destination= Station.KAOHSIUNG_STATION;
+//                break;
+//        }
+        List<Integer> list = new ArrayList<>();
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("Your start station?(1: Taipei, 2: Taichung, 3: Kaohsiung)");
-
+        int choice = Integer.parseInt(scanner.next());
+        Station start = null;
+        switch (choice) {
+            case 1:
+                start = Station.TAIPEI_STATION;
+                break;
+            case 2:
+                start = Station.TAICHUNG_STATION;
+                break;
+            case 3:
+                start = Station.KAOHSIUNG_STATION;
+                break;
+        }
+        System.out.println("Your destination station?(1: Taipei, 2: Taichung, 3: Kaohsiung)");
+        choice = Integer.parseInt(scanner.next());
+        Station destination = null;
+        switch (choice) {
+            case 1:
+                destination = Station.TAIPEI_STATION;
+                break;
+            case 2:
+                destination = Station.TAICHUNG_STATION;
+                break;
+            case 3:
+                destination = Station.KAOHSIUNG_STATION;
+                break;
+        }
+        Ticket ticket = new Ticket(start, destination);
+        ticket.print();
     }
 }
 
--- src/main/java/ticket/Ticket.java
+++ src/main/java/ticket/Ticket.java
@@ -11,9 +11,38 @@ public class Ticket {
     public Ticket(Station start, Station destination) {
         this.start = start;
         this.destination = destination;
+        if (start == Station.TAIPEI_STATION) {
+            if (destination == Station.TAICHUNG_STATION) {
+                price = 600;
+            } else {
+                price = 1500;
+            }
+        } else if (start == Station.TAICHUNG_STATION) {
+            if (destination == Station.TAIPEI_STATION) {
+                price = 600;
+            } else {
+                price = 900;
+            }
+        } else if (start == Station.KAOHSIUNG_STATION) {
+            if (destination == Station.TAICHUNG_STATION) {
+                price = 900;
+            } else {
+                price = 1500;
+            }
+        }
     }
 
     public void print() {
-//        taipeiStation = 105;
+//        if((start.id == 100 && destination.id == 300) || (start.id == 300 && destination.id == 100) ){
+//            price = 1500;
+//        }
+//        else if((start.id == 100 && destination.id == 200) || (start.id == 300 && destination.id == 200) ){
+//            price = 600;
+//        }
+//        else if((start.id == 300 && destination.id == 500) || (start.id == 500 && destination.id == 300) ){
+//            price = 900;
+//        }
+//        System.out.println(price);
+        System.out.println(start.name + "\t" + destination.name + "\t" + price );
     }
 }
