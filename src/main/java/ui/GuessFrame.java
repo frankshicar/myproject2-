ed6dd26fca88bb77deca7911e0fa41a23be70ee9
frankshicar <93965977+frankshicar@users.noreply.github.com>
2022-05-27 16:26:17 +0800

    Homework1

--- src/main/java/ui/GuessFrame.java
+++ src/main/java/ui/GuessFrame.java
@@ -4,6 +4,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
+import java.util.Scanner;
 
 public class GuessFrame extends JFrame {
     public GuessFrame(){
@@ -11,15 +13,26 @@ public class GuessFrame extends JFrame {
         setSize(500,500);
         setLocation(100,500);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-        JButton button = new JButton("Hi");
-        JLabel label = new JLabel("zzz");
+        JButton button = new JButton("猜");
+        JLabel label = new JLabel("猜1到100之間的數字");
+        JTextField number = new JTextField(8);
         button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                label.setText("Hello!");
+                int guessnum = Integer.parseInt(number.getText());
+                Random random = new Random();
+                int ans = random.nextInt(100) + 1;
+                if(ans == guessnum){
+                    label.setText("Bingo!");
+                }else if( guessnum < ans){
+                    label.setText("Bigger, answer is " + guessnum + " to 100");
+                }else{
+                    label.setText("smaller, answer is 1 to " + guessnum);
+                }
             }
         });
         setLayout(new FlowLayout());
+        add(number);
         add(button);
         add(label);
         setVisible(true);
