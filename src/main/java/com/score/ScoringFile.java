dc1907b96c648ba265d66ae8fef3dd44795f31a0
frankshicar <93965977+frankshicar@users.noreply.github.com>
2022-06-17 16:31:22 +0800

    new commit

--- src/main/java/com/score/ScoringFile.java
+++ src/main/java/com/score/ScoringFile.java
@@ -9,15 +9,29 @@ public class ScoringFile {
     public static void main(String[] args) {
         try {
             FileReader fileReader = new FileReader("scores.txt");
-            int data = fileReader.read();
-            System.out.println(data);
-            data = fileReader.read();
-            System.out.println(data);
+            BufferedReader reader = new BufferedReader(fileReader);
+            String line = reader.readLine();
+            while(line != null) {
+                String[] token = line.split(",");
+                String name = token[0];
+                int english = Integer.parseInt(token[1]);
+                int math = Integer.parseInt(token[2]);
+                Student stu = new Student(name, english, math);
+                stu.print();
+                line = reader.readLine();
+            }
+
+
+//            int data = fileReader.read();
+//            while(data != -1) {
+//                System.out.print((char)data);
+//                data = fileReader.read();
+//            }
         } catch (FileNotFoundException e) {
             System.out.println("檔案讀取失敗");
         } catch (IOException e) {
             System.out.println("資料讀取失敗");
         }
-        System.out.println("Testing");
+        System.out.println("Continuing");
     }
 }
